dff543672e0725652b46097d033a983aeb071b10
Edgar Gago Carrillo <edgargc.upc@gmail.com>
2022-11-29 08:29:11 +0100

    feat(core-components): added logo component

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+    browser: true
+  },
+  parser: '@babel/eslint-parser',
+  extends: ['next/core-web-vitals', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+    project: './jsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+      experimentalObjectRestSpread: true
+    }
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/display-name': 'off',
+    '@next/next/no-img-element': 'off',
+    'react/no-unescaped-entities': 'off',
+    'import/no-anonymous-default-export': 'off',
+
+    // add new line above comment
+    'lines-around-comment': 'off',
+
+    // add new line above return
+    'newline-before-return': 'off',
+
+    // add new line below import
+    'import/newline-after-import': 'off',
+
+    // add new line after each var, const, let declaration
+    'padding-line-between-statements': 'off'
+  }
+}
