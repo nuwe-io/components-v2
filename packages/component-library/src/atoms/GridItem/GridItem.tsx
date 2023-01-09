3d6be9a9a326131d7e33592ae0d8896edf2c85f4
AdrianNuwe <adrian@nuwe.io>
2023-01-09 12:13:47 +0100

    fix: corrections for business migration

--- packages/component-library/src/atoms/GridItem/GridItem.tsx
+++ packages/component-library/src/atoms/GridItem/GridItem.tsx
@@ -1,13 +1,15 @@
 import Grid from '@mui/material/Grid'
 
 interface GridItemProps {
+  xs?: number
   sm?: number
   lg?: number
+  xl?: number
   children: React.ReactNode
 }
 
 export const GridItem = (props: GridItemProps) => (
-  <Grid item xs={12} sm={props.sm} lg={props.lg}>
+  <Grid item xs={props.xs || 12} sm={props.sm} lg={props.lg} xl={props.xl || 5}>
     {props.children}
   </Grid>
 )
