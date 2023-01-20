e5394e3ac9553a3b7557bd67999d80bd91bc3a4a
AdrianNuwe <adrian@nuwe.io>
2023-01-20 01:10:07 +0100

    fix: 🐛 business theme config

--- packages/materio/src/configs/themeConfig.ts
+++ packages/materio/src/configs/themeConfig.ts
@@ -37,3 +37,40 @@ export const themeConfig = {
   toastPosition:
     'top-right' /* top-left | top-center | top-right | bottom-left | bottom-center | bottom-right */
 }
+
+const businessThemeConfig = {
+  // ** Layout Configs
+  templateName: 'Nuwe' /* App Name */,
+  layout: 'horizontal' /* vertical | horizontal */,
+  mode: 'dark' /* light | dark */,
+  direction: 'ltr' /* ltr | rtl */,
+  skin: 'default' /* default | bordered | semi-dark /*! Note: semi-dark value will only work for Vertical Layout */,
+  contentWidth: 'boxed' /* full | boxed */,
+  footer: 'static' /* fixed | static | hidden */,
+  // ** Routing Configs
+  routingLoader: true /* true | false */,
+  // ** Navigation (Menu) Configs
+  navHidden: true /* true | false */,
+  menuTextTruncate: true /* true | false */,
+  navSubItemIcon: CircleOutline /* Icon Element */,
+  verticalNavToggleType:
+    'accordion' /* accordion | collapse /*! Note: This is for Vertical navigation menu only */,
+  navCollapsed: false /* true | false /*! Note: This is for Vertical navigation menu only */,
+  navigationSize: 260 /* Number in PX(Pixels) /*! Note: This is for Vertical navigation menu only */,
+  collapsedNavigationSize: 68 /* Number in PX(Pixels) /*! Note: This is for Vertical navigation menu only */,
+  afterVerticalNavMenuContentPosition: 'fixed' /* fixed | static */,
+  beforeVerticalNavMenuContentPosition: 'fixed' /* fixed | static */,
+  horizontalMenuToggle:
+    'hover' /* click | hover /*! Note: This is for Horizontal navigation menu only */,
+  horizontalMenuAnimation: true /* true | false */,
+  // ** AppBar Configs
+  appBar:
+    'fixed' /* fixed | static | hidden /*! Note: hidden value will only work for Vertical Layout */,
+  appBarBlur: true /* true | false */,
+  // ** Other Configs
+  responsiveFontSizes: true /* true | false */,
+  disableRipple: false /* true | false */,
+  disableCustomizer: TruckCheckOutline /* true | false */,
+  toastPosition:
+    'top-right' /* top-left | top-center | top-right | bottom-left | bottom-center | bottom-right */
+}
