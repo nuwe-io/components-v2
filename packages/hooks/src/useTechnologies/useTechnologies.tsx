e709100b1f1c06e3cb60bd9b359bd18bf8bc5c97
Edgar Gago Carrillo <edgargc.upc@gmail.com>
2023-01-28 00:09:04 +0100

    fix(hooks): 🐛 fix undefined open value

--- packages/hooks/src/useTechnologies/useTechnologies.tsx
+++ packages/hooks/src/useTechnologies/useTechnologies.tsx
@@ -16,23 +16,25 @@ export const TechnologiesProvider = ({
 }) => {
   const { getAllTechnologies } = technologiesServices(technologiesRepository(BACKEND_URL))
 
-  const { data, error } = useSWR(open ? 'technologies' : null, getAllTechnologies)
-
-  const value = {
-    technologies: data && data.data.data,
-    loading: !data && !error,
-    error: data?.error
-  }
-
-  return <TechnologiesContext.Provider value={value}>{children}</TechnologiesContext.Provider>
+  return (
+    <TechnologiesContext.Provider value={getAllTechnologies}>
+      {children}
+    </TechnologiesContext.Provider>
+  )
 }
 
-export const useTechnologies = () => {
+export const useTechnologies = (open: boolean) => {
   const context = React.useContext(TechnologiesContext)
 
   if (context === undefined) {
     throw new Error('useTechnologies must be used within a TechnologiesProvider')
   }
 
-  return context
+  const { data, error } = useSWR(open ? 'technologies' : null, context)
+
+  return {
+    technologies: data && data.data.data,
+    loading: !data && !error,
+    error: data?.error
+  }
 }
