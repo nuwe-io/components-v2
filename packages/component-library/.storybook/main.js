8937894746815f806e42d617d1e133e97d7cf806
Edgar Gago Carrillo <edgargc.upc@gmail.com>
2023-02-07 08:15:06 +0100

    chore(cl): 🔧 upgrade storybook and add webpack final config to main

--- packages/component-library/.storybook/main.js
+++ packages/component-library/.storybook/main.js
@@ -1,8 +1,21 @@
+const path = require('path')
+
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5'
+  },
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@atoms': path.resolve(__dirname, '../src/atoms'),
+      '@molecules': path.resolve(__dirname, '../src/molecules'),
+      '@organisms': path.resolve(__dirname, '../src/organisms'),
+      '@templates': path.resolve(__dirname, '../src/templates')
+    }
+
+    return config
   }
 }
--- packages/component-library/rollup.config.js
+++ packages/component-library/rollup.config.js
@@ -1,15 +1,14 @@
 import commonjs from '@rollup/plugin-commonjs'
-import resolve from '@rollup/plugin-node-resolve'
-import typescript from '@rollup/plugin-typescript'
-import external from 'rollup-plugin-peer-deps-external'
+import resolve from 'rollup-plugin-node-resolve'
 import postcss from 'rollup-plugin-postcss'
 import scss from 'rollup-plugin-scss'
 import { terser } from 'rollup-plugin-terser'
+import typescript from 'rollup-plugin-ts'
 import visualizer from 'rollup-plugin-visualizer'
 
 import pkg from './package.json'
 
-const extensions = ['.ts', '.tsx']
+const extensions = ['.js', '.jsx', '.ts', '.tsx']
 
 export default {
   input: './src/index.ts',
@@ -30,13 +29,19 @@ export default {
       filename: './bundle-report.html',
       open: false
     }),
-    external(),
-    resolve(),
+    resolve({
+      extensions,
+      modulesOnly: true
+    }),
     commonjs(),
     typescript({
-      tsconfig: './tsconfig.json'
+      tsconfig: './tsconfig.json',
+      transpiler: 'babel'
     }),
-    postcss(),
+    postcss({
+      plugins: [],
+      minimize: true
+    }),
     scss(),
     terser()
   ],
